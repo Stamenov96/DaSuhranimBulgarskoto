7355167364e9d6696a3d330d68271a582a7c658e
Stamenov96 <stefan.stamenov1996@gmail.com>
2015-03-14 14:58:55 +0200

    Added other main categories html files

--- WebContent/scripts/maincats.js
+++ WebContent/scripts/maincats.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
 	"use strict"
 
+	var a = {"Ё":"YO","Й":"I","Ц":"TS","У":"U","К":"K","Е":"E","Н":"N","Г":"G","Ш":"SH","Щ":"SCH","З":"Z","Х":"H","Ъ":"'","ё":"yo","й":"i","ц":"ts","у":"u","к":"k","е":"e","н":"n","г":"g","ш":"sh","щ":"sch","з":"z","х":"h","ъ":"'","Ф":"F","Ы":"I","В":"V","А":"a","П":"P","Р":"R","О":"O","Л":"L","Д":"D","Ж":"ZH","Э":"E","ф":"f","ы":"i","в":"v","а":"a","п":"p","р":"r","о":"o","л":"l","д":"d","ж":"zh","э":"e","Я":"Ya","Ч":"CH","С":"S","М":"M","И":"I","Т":"T","Ь":"'","Б":"B","Ю":"YU","я":"ya","ч":"ch","с":"s","м":"m","и":"i","т":"t","ь":"'","б":"b","ю":"yu"};
+
+	function transliterate(word){
+	  return word.split('').map(function (char) { 
+	    return a[char] || char; 
+	  }).join("");
+	}
+	
+	
 	function handleError(error) {
 		console.error("error", error, arguments);
 	}
 	
 	function appendToList(list, post) {
-		var newElement = $("<li><a href="+post.title+".hmtl>"+post.title+"</a></li>");
+		var newElement = $("<li><a href="+transliterate(post.title)+".html>"+post.title+"</a></li>");
 		//newElement.text(post.title);
 		list.append(newElement);
 	}
