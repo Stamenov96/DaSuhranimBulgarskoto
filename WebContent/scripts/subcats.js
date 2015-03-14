8da5159425cfd5e0a6ebd79493cb8867f0f8717a
Stamenov96 <stefan.stamenov1996@gmail.com>
2015-03-14 20:21:22 +0200

    Functional info button for "Severnyashka.html"

--- WebContent/scripts/subcats.js
+++ WebContent/scripts/subcats.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
 	"use strict"
 
+	
+	
+	
+	
 	$("#severinfo").click(function() {
 		$.ajax('http://private-9ece4-dasuhranimbulgarskoto.apiary-mock.com/category/1/subcat/1', {
 			  method: "GET"
 			}).then(function(data) {
 				var pars = JSON.stringify(data);
-				//console.log(pars);
+				console.log(pars);
 				pars= pars.split(":");
 				//var par = JSON.parse(pars);
 				alert(pars[3].substr(0, pars[3].length-1) );
