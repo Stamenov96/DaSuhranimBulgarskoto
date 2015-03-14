6a199990faba657e42bb1369a36a46e4a20fb4a4
Stamenov96 <stefan.stamenov1996@gmail.com>
2015-03-14 20:14:26 +0200

    Add sub categories script

---
+++ WebContent/scripts/subcats.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	"use strict"
+
+	$("#severinfo").click(function() {
+		$.ajax('http://private-9ece4-dasuhranimbulgarskoto.apiary-mock.com/category/1/subcat/1', {
+			  method: "GET"
+			}).then(function(data) {
+				var pars = JSON.stringify(data);
+				//console.log(pars);
+				pars= pars.split(":");
+				//var par = JSON.parse(pars);
+				alert(pars[3].substr(0, pars[3].length-1) );
+				return false;
+				//console.log(pars);
+			});
+	
+	});
+	
+	
+	
+	
+	
+});
