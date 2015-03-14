94ad4fcc6068d98599f99edd607437efda58e6f8
Stamenov96 <stefan.stamenov1996@gmail.com>
2015-03-14 16:46:26 +0200

    Added a javascript for "Tansi"

---
+++ WebContent/scripts/tantsi.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	"use strict"
+
+	
+	$("#tantsiinfo").click(function() {
+		$.ajax('http://private-9ece4-dasuhranimbulgarskoto.apiary-mock.com/category/1', {
+			  method: "GET"
+			}).then(function(data) {
+				var pars = JSON.stringify(data);
+				//console.log(pars);
+				pars= pars.split(":");
+				//var par = JSON.parse(pars);
+				alert(pars[3].substr(0, pars[3].length-1) );
+				return false;
+				//console.log(pars);
+			});
+	
+	});
+	
+});
