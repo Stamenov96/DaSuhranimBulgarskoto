edd956ed6eb1e72d7ad623d9a33f52e202ebda7f
Stamenov96 <stefan.stamenov1996@gmail.com>
2015-03-28 12:24:13 +0200

    Add a Main Category and view info for single Main Category

--- src/com/dasuhranimbulgarskoto/blog/rest/MainCaregoryRest.java
+++ src/com/dasuhranimbulgarskoto/blog/rest/MainCaregoryRest.java
@@ -3,7 +3,9 @@ package com.dasuhranimbulgarskoto.blog.rest;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -11,6 +13,8 @@ import com.dasuhranimbulgarskoto.blog.service.MainCategoryService;
 import com.dasuhranimbulgarskoto.blog.service.Services;
 import com.dasuhranimbulgarskoto.blog.models.MainCategory;
 
+
+@Path("MainCategories")
 public class MainCaregoryRest {
 
 	private final MainCategoryService mainCategoryService;
@@ -25,5 +29,21 @@ public class MainCaregoryRest {
 	public List<MainCategory> getMainCategories(){
 		return mainCategoryService.getMainCategories();
 	}
+	
+	@GET
+	@Path("/{mainCategoryId}")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public MainCategory getMainCategory(@PathParam("mainCategoryId") long mainCategoryId ){
+		return mainCategoryService.getMainCategory(mainCategoryId);
+		
+	}
+		
+	@POST
+	@Path("/")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public MainCategory createMainCategory(MainCategory mainCategory){
+		return mainCategoryService.createMainCategory(mainCategory);
+	}
+	
 	
 }
